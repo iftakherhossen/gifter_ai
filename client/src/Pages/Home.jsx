1cdbb3236b21f847df7c8f0b9823b9829484ce89
Iftakher Hossen <iftakher.one@gmail.com>
2023-03-07 11:01:59 +0600

    adding env variables

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           setLoading(true);
 
           try {
-               const response = await fetch('http://localhost:8080/api/v1/post', {
+               const response = await fetch(`${import.meta.env.VITE_SERVER_LINK}/api/v1/post`, {
                     method: 'GET',
                     headers: {
                          'Content-Type': 'application/json',
